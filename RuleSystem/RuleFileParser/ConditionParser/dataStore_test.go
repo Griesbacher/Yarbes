ffb007cc02e36e9d76216b923504acf0ed34176f
Philip Griesbacher <philip@griesbacher.org>
2015-11-05 14:41:10 +0100

    added test

---
+++ RuleSystem/RuleFileParser/ConditionParser/dataStore_test.go
@@ -0,0 +1,34 @@
+package ConditionParser
+
+import (
+	"testing"
+	"go/ast"
+	"go/token"
+)
+
+//TestReturnResult is a dummy test because the PrintNode is just for debugging
+func TestReturnResult(t *testing.T) {
+	t.Parallel()
+	d := dataStore{result:[]bool{}}
+	if d.returnResult() {
+		t.Error("Result should be false on empty list")
+	}
+	d = dataStore{result:[]bool{}, stack:[]ast.Node{&ast.BasicLit{Kind:token.COMMENT}}}
+	if !didThisPanic(d.returnResult) {
+		t.Error("returnResult should panic on false Asttype")
+	}
+	d = dataStore{result:[]bool{}, stack:[]ast.Node{&ast.Comment{}}}
+	if !didThisPanic(d.returnResult) {
+		t.Error("returnResult should panic on false Tokentype")
+	}
+}
+
+func didThisPanic(f func() (bool)) (result bool) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			result = true
+		}
+	}()
+	f()
+	return false
+}
